bfa2fec9cb642e66501197d8ba696bfe94b9ae84
cyb3r_anarch <36559571+tiropas@users.noreply.github.com>
2018-09-24 21:14:30 -0700

    Add files via upload

---
+++ Assets/scripts/Player_Collusion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player_Collusion : MonoBehaviour {
+
+    public player_movment moving;
+
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+        if (collisionInfo.collider.tag == "problem")
+        {
+            moving.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ Assets/scripts/player_movment.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movment : MonoBehaviour {
+    public float forwardForce = 8000f;
+    public float sidewaysForce = 600f;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb.AddForce(0,10,1500);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0,0,1000 * Time.deltaTime);
+
+        if (Input.GetKey("right"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("left"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("up"))
+        {
+            rb.AddForce(0, 0, 1500 * Time.deltaTime);
+        }
+        if (Input.GetKey("down"))
+        {
+            rb.AddForce(0, 0, -1500 * Time.deltaTime);
+        }
+        
+    }
+}
